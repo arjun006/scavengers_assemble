47bea6a1d482288bf5127d61682dbdfb3223d3a7
Shira <wisdomkangngo@gmail.com>
2021-05-01 13:12:23 -0400

    Question changes
    changes

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,54 +1,56 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, ImageBackground, Image, Button } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  ImageBackground,
+  Image,
+  Button,
+} from "react-native";
 import * as Permissions from "expo-permissions";
-import colours from '../config/colours';
+import colours from "../config/colours";
 
 export default function HomeScreen() {
-    useEffect(() => {
-        (async () => {
-          const { status } = await Camera.requestPermissionsAsync();
-          setHasPermission(status === "granted");
-        })();
-      }, []);
-    
-      if (hasPermission === null) {
-        return <View />;
-      }
-      if (hasPermission === false) {
-        return <Text>No access to camera</Text>;
-      }
+  const [hasPermission, setHasPermission] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
     <>
-    <View style={{backgroundColor: styles.background.backgroundColor}}>
-    </View>
-    <ImageBackground style={styles.background}>
-    <View style={styles.logoContainer}>
-        <Image
-        
-        source={require('../assets/Title.png')}/>
-    </View>
-    <View>
-        <Button
-
-        />
-    </View>
-    </ImageBackground>
+      <View
+        style={{ backgroundColor: styles.background.backgroundColor }}
+      ></View>
+      <ImageBackground style={styles.background}>
+        <View style={styles.logoContainer}>
+          <Image source={require("../assets/Title.png")} />
+        </View>
+        <View>
+          <Button />
+        </View>
+      </ImageBackground>
     </>
-
   );
- 
-
 }
 const styles = StyleSheet.create({
-    background: {
-        flex: 1,
-        alignItems:'center',
-        backgroundColor: colours.red
-    },
-    logoContainer: {
-        padding: 10,
-        justifyContent: "flex-start",
-        marginTop: '55%'
-
-    }
-})
+  background: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: colours.red,
+  },
+  logoContainer: {
+    padding: 10,
+    justifyContent: "flex-start",
+    marginTop: "55%",
+  },
+});
