ffb164bd0f594a5d9f6a1bd77cc07b33e3b973a6
arjun006 <arjun.devakumar66@gmail.com>
2021-05-01 02:02:46 -0400

    firebase dependencies

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { View, Text } from "react-native";
 import HomeScreen from './app/screens/HomeScreen';
+import * as firebase from 'firebase';
 
 export default function App() {
   return (
