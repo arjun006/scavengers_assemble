3bd8512ff4a331a5d04b5b38c26376018c907439
arjun006 <arjun.devakumar66@gmail.com>
2021-05-01 11:23:34 -0400

    fixed

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 import React, { useEffect, useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import { LoginScreen, HomeScreen, RegistrationScreen } from './app/screens'
+import { LoginScreen, HomeScreen, LoadingScreen, LobbyScreen, LeaderBoardScreen } from './app/screens'
 import {decode, encode} from 'base-64'
 if (!global.btoa) {  global.btoa = encode }
 if (!global.atob) { global.atob = decode }
