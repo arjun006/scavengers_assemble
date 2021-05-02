977703ffc5d4d968152b35cb85bbd2b0ca9c3141
Jenil-Vekaria <jvekaria@ryerson.ca>
2021-05-02 19:27:10 -0400

    Added screenshot

--- app/screens/QuestionScreen.js
+++ app/screens/QuestionScreen.js
@@ -151,7 +151,7 @@ export default function QuestionScreen({ route, navigation }) {
           <CountdownCircleTimer
             size={70}
             isPlaying={isPlaying}
-            duration={10}
+            duration={20}
             colors={[
               ["#00FF00", 0.83],
               ["#FF8C00", 0.17],
